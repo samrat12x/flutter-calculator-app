36daa534f5f643790ab1cc7ad93bb29cb2191065
samrat patel <samratpatel12x@gmail.com>
2023-11-20 22:18:58 +0530

    1st :just initialising  git

---
+++ lib/app/w1.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "flutter course: first appp",
+      home: Material(
+        child: Column(children: [
+          Row(
+            children: [
+              Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+              Icon(
+                Icons.audiotrack,
+                color: Colors.green,
+                size: 30.0,
+              ),
+              Icon(
+                Icons.beach_access,
+                color: Colors.blue,
+                size: 36.0,
+              ),
+            ],
+          ),
+          Text("hello polo")
+        ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fuck/app/w1.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(Widget1());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Widget1 extends StatelessWidget {
+  const Widget1({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MainApp();
   }
 }
