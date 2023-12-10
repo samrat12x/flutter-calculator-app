c11d8347e5c7d132fc451a6d7dc8a4dfbac88341
samrat patel <samratpatel12x@gmail.com>
2023-12-10 15:27:16 +0530

    fixed buttons orientation

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -63,11 +63,17 @@ class _CalculatorViewState extends State<CalculatorView> {
             keyboardType: TextInputType.number,
             inputFormatters: [numericFormatter], // Use the numeric formatter
             decoration: InputDecoration(
-              labelText: "Input 1",
-              errorText: errorMessage1,
+                labelText: "Input 1",
+                errorText: errorMessage1,
+                errorStyle: const TextStyle(color: Colors.black),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 35, 32, 32)),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                )
 
-              // ... (your existing decoration properties)
-            ),
+                // ... (your existing decoration properties)
+                ),
           ),
           const SizedBox(height: 15),
           TextField(
@@ -87,13 +93,21 @@ class _CalculatorViewState extends State<CalculatorView> {
             keyboardType: TextInputType.number,
             inputFormatters: [numericFormatter],
             decoration: InputDecoration(
-              labelText: "Input 2",
-              errorText: errorMessage2,
-              // ... (your existing decoration properties)
-            ),
+                labelText: "Input 2",
+                errorText: errorMessage2,
+                errorStyle: const TextStyle(color: Colors.black),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 35, 32, 32)),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                )
+
+                // ... (your existing decoration properties)
+                ),
           ),
           const SizedBox(height: 15),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               OutlinedButton(
                 style: outlineButtonStyle,
@@ -138,11 +152,26 @@ class _CalculatorViewState extends State<CalculatorView> {
               ),
             ],
           ),
+          const SizedBox(height: 15),
           TextField(
             showCursor: true,
             readOnly: true,
-            decoration:
-                InputDecoration(hintText: result == null ? '' : '$result'),
+            enabled: false,
+            decoration: InputDecoration(
+              hintText: result == null ? 'Output Screen' : '$result',
+//borders
+
+              disabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(width: 1, color: Colors.black)),
+            ),
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              foreground: Paint()
+                ..style = PaintingStyle.fill
+                ..color = Colors.black
+                ..strokeWidth = 2,
+              color: null,
+            ),
           ),
         ],
       ),
